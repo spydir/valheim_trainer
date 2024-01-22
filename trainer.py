cc372c002f7ff2c35f46f874d4a2a927b4686421
spydir <lee@sultgroup.com>
2024-01-22 10:15:36 -0500

    added licensing and refactored architecture.

--- trainer.py
+++ trainer.py
@@ -1,5 +1,6 @@
 import argparse
 import subprocess
+import keypress
 
 # Define the path to the keypress.py script
 KEYPRESS_SCRIPT_PATH = "keypress.py"
@@ -7,8 +8,8 @@ window_title = "Valheim"
 
 def run_keypress_script(keystroke_to_send, num_keystrokes, delay_between_keypresses):
     # Execute the keypress.py script with the specified parameters
-    subprocess.run(["python", KEYPRESS_SCRIPT_PATH, window_title, keystroke_to_send, str(num_keystrokes), str(delay_between_keypresses)])
-
+    keypress.send_keystrokes(window_title, keystroke_to_send, str(num_keystrokes), str(delay_between_keypresses))
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Valheim Trainer")
     parser.add_argument("action", choices=["jump", "run", "sneak"], help="Choose the action to perform")
@@ -18,11 +19,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.action == "jump":
-        run_keypress_script("space", 3, 2.8)
-
+        keypress.send_keystrokes(window_title, "space", 3, 2.8)
+    
     elif args.action == "run":
+        keypress.send_keystrokes(window_title, "w", 3, 10)
         run_keypress_script("w", 3, 10)
 
     elif args.action == "sneak":
-        run_keypress_script("ctrl", 1, 1)
-        run_keypress_script("w", 4, 1)
+        keypress.send_keystrokes(window_title, "ctrl", 1, 1)
+        keypress.send_keystrokes(window_title, "w", 3, 10)
